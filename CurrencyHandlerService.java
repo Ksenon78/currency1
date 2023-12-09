d4c97138c2272fc5a0835a8590b2a9fb8cdb8c18
Ksenon78 <139355903+Ksenon78@users.noreply.github.com>
2023-12-09 15:54:00 +0300

    CurrencyHandlerService.java

--- CurrencyHandlerService.java
+++ CurrencyHandlerService.java
@@ -2,6 +2,7 @@ package currency.example.currency.outbondhandler;
 
 import currency.example.currency.entity.CurrencyEntity;
 import currency.example.currency.mapper.CurrencyMapper;
+import currency.example.currency.mapper.CurrencyResponseMapper;
 import currency.example.currency.repository.CurrencyRepository;
 import currency.example.currency.outbound.CurrencyApiResponse;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,44 +10,49 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class CurrencyHandlerService {
 
     private final CurrencyMapper currencyMapper;
+    private final CurrencyResponseMapper currencyResponseMapper;
     private final CurrencyRepository currencyRepository;
 
 
     @Value("${currencylayer.api.key}")
     private String apiKey;
 
-    public CurrencyHandlerService(CurrencyMapper currencyMapper, CurrencyRepository currencyRepository) {
+    public CurrencyHandlerService(CurrencyMapper currencyMapper, CurrencyRepository currencyRepository, CurrencyResponseMapper currencyResponseMapper) {
         this.currencyMapper = currencyMapper;
         this.currencyRepository = currencyRepository;
+        this.currencyResponseMapper = currencyResponseMapper;
     }
 
     @Scheduled(fixedRate = 100000)
     public void fetchAndSaveCurrencyRatesScheduled() {
-        //fetchDataAndSaveToDatabase();
-        System.out.println("iptal ettin");
+        fetchDataAndSaveToDatabase();
+
     }
 
     public void fetchDataAndSaveToDatabase() {
-        //  String apiUrl = "https://api.currencylayer.com/live?access_key=" + apiKey;
-        String apiUrl = "http://apilayer.net/api/live?access_key=eaa4b7eb2120914faa950bfbf3438c4b&currencies=EUR,GBP,CAD,PL&source=USD&format=1";
-        RestTemplate restTemplate = new RestTemplate();
-        CurrencyApiResponse response = restTemplate.getForObject(apiUrl, CurrencyApiResponse.class);
-        CurrencyEntity entity = currencyMapper.toEntity(response);
-        currencyRepository.save(entity);
-    }
 
-    private void validateResponse(CurrencyApiResponse response) {
-        if (response == null) {
-            System.out.println("Reponse is null");
-            throw new RuntimeException("CurrencyApiResponse is null");
-        } else if (response.getSuccess() == false) {
-            System.out.println("Resonse status is" + response.getSuccess());
-            throw new RuntimeException("Resonse status is" + response.getSuccess());
-        }
+        String apiUrlUsd = "http://apilayer.net/api/live?access_key=eaa4b7eb2120914faa950bfbf3438c4b&currencies=EUR,GBP,CAD,PL&source=USD&format=1";
+        String apiUrlEur = "http://apilayer.net/api/live?access_key=eaa4b7eb2120914faa950bfbf3438c4b&currencies=USD,GBP,CAD,PL,TRY&source=EUR&format=1";
+
+        RestTemplate restTemplateUsd = new RestTemplate();
+
+        CurrencyApiResponse responseUsd = restTemplateUsd.getForObject(apiUrlUsd, CurrencyApiResponse.class);
+        CurrencyApiResponse responseEur = restTemplateUsd.getForObject(apiUrlEur, CurrencyApiResponse.class);
+        List<CurrencyEntity> currencyEntityList = new ArrayList<>();
+        currencyEntityList.add(currencyMapper.toEntity(responseUsd));
+        currencyEntityList.add(currencyMapper.toEntity(responseEur));
+
+
+
+
+        currencyRepository.saveAll(currencyEntityList);
 
     }
 
