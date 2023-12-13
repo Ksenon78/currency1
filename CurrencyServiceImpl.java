e4ed816b99a80838e2a4167ca18bf3d48fadb4d7
Ksenon78 <139355903+Ksenon78@users.noreply.github.com>
2023-12-13 23:04:49 +0300

    CurrencyServiceImpl.java

--- CurrencyServiceImpl.java
+++ CurrencyServiceImpl.java
@@ -1,35 +1,31 @@
 package currency.example.currency.service.serviceimpl;
 
 import currency.example.currency.entity.CurrencyEntity;
+import currency.example.currency.mapper.CurrencyMapper;
 import currency.example.currency.repository.CurrencyRepository;
 import currency.example.currency.response.CurrencyResponse;
 import currency.example.currency.service.CurrencyService;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class CurrencyServiceImpl implements CurrencyService {
     private final CurrencyRepository currencyRepository;
+    private final CurrencyMapper currencyMapper;
 
-    public CurrencyServiceImpl(CurrencyRepository currencyRepository) {
+    public CurrencyServiceImpl(CurrencyRepository currencyRepository, CurrencyMapper currencyMapper) {
         this.currencyRepository = currencyRepository;
+        this.currencyMapper = currencyMapper;
     }
 
     @Override
-    public List<CurrencyResponse> findAllCurrency(String currencyType, String currencySource, Date currencyTime) {
+    public List<CurrencyResponse> findAllCurrency(String currencyType, String currencySource, LocalDate currencyTime) {
         List<CurrencyEntity> allByPrivacyAndTimestampAndSource = currencyRepository.findAllByPrivacyAndTimestampAndSource(currencySource, currencyTime, currencyType);//TODO Hepsi gelirse
-
-
-      return allByPrivacyAndTimestampAndSource.stream()
-                .map(currencyResponseMapper::toResponse)
-                .collect(Collectors.toList());
+        List<CurrencyResponse> list = allByPrivacyAndTimestampAndSource.stream().map(entity -> currencyMapper.toResponse(entity)).toList();
+        return list;
     }
-
-
-
-
 
 
 }
